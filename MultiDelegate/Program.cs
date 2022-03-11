ae0502b9f945fc648a270d001fa2f412d5885ccc
Tibo De Vuyst <Tibo.DeVuyst@student.intecbrussel.be>
2022-03-11 10:18:30 +0100

    Add project files.

---
+++ MultiDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDelegate
+{
+    public delegate void RecDelegate(double x, double y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            RecDelegate recDelegate = new RecDelegate(p.GetArea);
+            recDelegate += p.GetPerimeter;
+            recDelegate.Invoke(12.55, 5.60);
+            //recDelegate(12.55, 5.60);
+
+        }
+        public void GetArea(double width,double height)
+        {
+            Console.WriteLine( width * height);
+        }
+        public void GetPerimeter(double width, double height)
+        {
+            Console.WriteLine(2*(width+height));
+        }
+    }
+}
---
+++ SimpleDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleDelegate
+{
+    public delegate void AddDelegate(int x, int y);
+    public delegate string SayDelegate(string str);
+    internal class Program
+    {
+        //1.simple
+        //2.Miltiple
+        //3.Genaric Delegate
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            AddDelegate ad = new AddDelegate(P.AddNumbers);
+            ad(2, 3);
+            SayDelegate sd = new SayDelegate(SayHello);
+            Console.WriteLine(sd("kenan"));
+
+            Console.ReadLine();
+        }
+        public void AddNumbers( int a , int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static string SayHello(string name)
+        {
+            return $"hello {name}";
+        }
+    }
+}
